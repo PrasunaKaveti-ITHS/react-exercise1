21595133e1ddbccfb45712eab2042cdbd973bcf9
PrasunaKaveti-ITHS <70919098+PrasunaKaveti-ITHS@users.noreply.github.com>
2021-05-12 14:46:02 +0200

    worked with displaying error message display

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,14 +1,34 @@
-const Menu = ()=>{
-    let content = null
-    return(
-        <div>
-            <nav>
-                klick
-            </nav>
-            <main>
-                {content}
-            </main>
-        </div>
-    )
+import { useState } from 'react'
+import NoImport from './NoImport'
+import CallFunctionInEvent from './CallFunctionInEvent'
+
+const Menu = () => {
+	const [selectedItem, setSelectedItem] = useState('')
+	const NO_IMPORT = 'forgot to import useState from React', CALL_FUNCTION = 'call function with () in event'
+
+	let content = null
+	if( selectedItem === NO_IMPORT ) {
+		content = <NoImport />
+	} else if( selectedItem === CALL_FUNCTION ) {
+		content = <CallFunctionInEvent />
+	}
+
+	function chooseNoImport() {
+		setSelectedItem(NO_IMPORT)
+	}
+
+	return (
+		<div className="menu">
+			<nav>
+				<button onClick={() => setSelectedItem('')}> Reset </button>
+				<button onClick={chooseNoImport}> No import </button>
+				<button onClick={() => setSelectedItem(CALL_FUNCTION)}> Call function with () in event </button>
+			</nav>
+			<main>
+				{content}
+			</main>
+		</div>
+	)
 }
+
 export default Menu
